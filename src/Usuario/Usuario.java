d212323e175e5cf0575f1c7befbcb8863826ff88
FranLongaretto <franlongaretto@gmail.com>
2022-06-08 01:11:57 -0300

    Correccion lectura y escritura en class Usuario

--- src/Usuario/Usuario.java
+++ src/Usuario/Usuario.java
@@ -2,12 +2,16 @@ package Usuario;
 
 import Flota.TipoAvion;
 import Interfaces.Archivos;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.io.*;
 import java.lang.reflect.Type;
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -105,69 +109,56 @@ public class Usuario implements Archivos<Usuario> {
     //Se hace override de la interfaz para tratar Archivos (en este caso de Usuarios)
     @Override
     public List<Usuario> leerArchivo() {
+        List<Usuario> listaUsuarios = null;
         File fileUsuarios = new File("Usuarios.json");
-        Type listArchUsuarios = new TypeToken<ArrayList<Usuario>>() {}.getType();
-        ArrayList<Usuario> userArray = null;
-        if (fileUsuarios.exists()) {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        if(fileUsuarios.exists() && fileUsuarios.canRead()) {
             try {
-                BufferedReader bufferedReader = new BufferedReader(new FileReader(fileUsuarios));
-                Gson gson = new Gson();
-                userArray = gson.fromJson(bufferedReader, listArchUsuarios);
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
+                listaUsuarios = Arrays.asList(mapper.readValue(fileUsuarios, Usuario[].class)); //Convierto Json array a list de usuarios
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } else {
+        }else{
             System.out.printf("El archivo no existe\n");
         }
-
-        return userArray;
+        return listaUsuarios;
     }
     @Override
     public void agregarEnArchivo() {
         File fileUsuarios = new File("Usuarios.json");
-        Type listArchUsuarios = new TypeToken<ArrayList<Usuario>>() {}.getType();
-        ArrayList<Usuario> userArray = null;
-        Gson gson = new Gson();
-        //Pregunto si el archivo existe
-        if (fileUsuarios.exists()){
-            try {
-                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileUsuarios));
-                userArray = new ArrayList<Usuario>(leerArchivo()); //Bajo el los datos del json con leerArchivo() y lo guardo en userArray
-                userArray.add(this); //Guardo en la lista de usuarios totales el nuevo usuario a registrar
-                gson.toJson(userArray, listArchUsuarios, bufferedWriter); //vuelvo a escribir el archivo JSON de Usuarios
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        try {
+            //Pregunto si el archivo existe
+            if (fileUsuarios.createNewFile()) {
+                ArrayList<Usuario> usuarioArrayList = new ArrayList<>();
+                usuarioArrayList.add(this);
+                mapper.writeValue(fileUsuarios, usuarioArrayList);
+            } else {
+                ArrayList<Usuario> userArrayList = new ArrayList<Usuario>(leerArchivo());
+                userArrayList.add(this);
+                mapper.writeValue(fileUsuarios, userArrayList);
             }
-        }else{
-            //Si no existe hago un array list vacio, guardo mi primer usuario y lo escribo en el archivo JSON.
-            try {
-                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileUsuarios));
-                userArray = new ArrayList<>();
-                userArray.add(this);
-                gson.toJson(userArray, listArchUsuarios, bufferedWriter);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
     @Override
     public void mostrarArchivo() {
         File fileUsuarios = new File("Usuarios.json");
-        Type listArchUsuarios = new TypeToken<ArrayList<Usuario>>() {}.getType();
-        ArrayList<Usuario> userArray = null;
-        if (fileUsuarios.exists()) {
-            try {
-                BufferedReader bufferedReader = new BufferedReader(new FileReader(fileUsuarios));
-                Gson gson = new Gson();
-                userArray = gson.fromJson(bufferedReader, listArchUsuarios);
-                for (Usuario elemento: userArray) {
-                    elemento.toString();
-                }
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        if(fileUsuarios.exists()) {
+            try{
+                List<Usuario> users = Arrays.asList(mapper.readValue(fileUsuarios, Usuario[].class)); //Convierto Json array a list de objetos
+                users.stream().forEach(obj -> System.out.println(obj)); // Mostrar lista de usuarios
+            } catch (IOException e) {
+                System.out.println("Error!!");
             }
-
+        }else{
+            System.out.println("EL archivo esta vacio");
         }
     }
 }
