ba5e593df9935b04f36bb81768a45cba91b0f57c
Nahuel-Iglesias <nahu.nahu86@gmail.com>
2022-06-13 19:22:28 -0300

    clase ticket + menu usuario

---
+++ src/Ticket/Ciudad.java
@@ -0,0 +1,13 @@
+package Ticket;
+
+import java.net.PortUnreachableException;
+
+public enum Ciudad {
+    BUENOSAIRES,
+    MONTEVIDEO,
+    CORDOBA,
+    SANTIAGO;
+
+    private Ciudad() {
+    }
+}
